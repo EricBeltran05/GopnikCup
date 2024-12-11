0267b22cb72a4880482fda54589ef10a5ee78e8e
PCERIC\eric_ <ebeltranfandos@gmail.com>
2024-12-11 18:25:58 +0100

    First commit

---
+++ GopnikCupApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Gopnik Cup APP"
+include(":app")
+ 
